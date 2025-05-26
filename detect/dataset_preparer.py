578942f4b7409c026350b083f1bec4a9e9a161dc
uni1285 <122028512+uni1285@users.noreply.github.com>
2025-05-26 23:14:56 +0900

    Update and rename dataset_preparer to dataset_preparer.py

--- detect/dataset_preparer.py
+++ detect/dataset_preparer.py
@@ -5,3 +5,4 @@ def prepare_yolo_data(image_dir, label_dir, dest_dir="yolo_data"):
     os.makedirs(dest_dir, exist_ok=True)
     shutil.copytree(image_dir, os.path.join(dest_dir, "images"), dirs_exist_ok=True)
     shutil.copytree(label_dir, os.path.join(dest_dir, "labels"), dirs_exist_ok=True)
+
